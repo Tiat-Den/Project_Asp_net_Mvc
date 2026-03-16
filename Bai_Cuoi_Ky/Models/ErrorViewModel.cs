749b4e62f377633e0a8826506d6c25c20a0d28e5
Đạt <datpham14563@gmail.com>
2026-03-16 12:42:43 +0700

    Khởi tạo project

---
+++ Bai_Cuoi_Ky/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bai_Cuoi_Ky.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
